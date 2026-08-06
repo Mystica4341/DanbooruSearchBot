a603e3bb0f2d4e528bd5370dda3b1cdf6f5812c5
Mirera <phudongsi1@gmail.com>
2026-08-06 17:04:13 +0700

    add comment for nsfw checking function

--- helpers/nsfw_check.py
+++ helpers/nsfw_check.py
@@ -1,3 +1,5 @@
+# General checking for NSFW content in channels
+# If need specific checking for commands, need to implement in the command itself in their respective modules
 async def isNSFW(interaction):
     if not interaction.channel.is_nsfw():
         await interaction.followup.send("NSFW content is not allowed in this channel.")
